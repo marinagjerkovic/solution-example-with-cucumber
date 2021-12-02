9df8379f6b5eb0b6380ea29c9071e05f2b7a42ea
Marina Jerkovic <marina.jerkovic@htecgroup.com>
2021-12-02 17:00:12 +0100

    Parallel execution

--- tpc-automation/src/test/java/runner/TestRunner.java
+++ tpc-automation/src/test/java/runner/TestRunner.java
@@ -2,19 +2,25 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {
-//                "src/test/resources/api-features"
-                "src/test/resources/ui-features"
+                "src/test/resources/api-features"
+                ,"src/test/resources/ui-features"
         }
         ,glue = {
-//                "api/steps"
-                "ui/steps"
+                "api/steps"
+                ,"ui/steps"
         }
         ,plugin = {
                 "pretty"
                 ,"html:target/cucumber-reports.html"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
+//        @Override
+//        @DataProvider(parallel = true)
+//        public Object[][] scenarios() {
+//                return super.scenarios();
+//        }
 }
